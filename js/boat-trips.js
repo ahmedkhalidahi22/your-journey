fa6fe4d7dcab802c176c75b511eecd6d67da6892
ahmedkhalidahi22 <79201796+ahmedkhalidahi22@users.noreply.github.com>
2021-08-03 00:31:05 +0200

    Update boat-trips.js

--- js/boat-trips.js
+++ js/boat-trips.js
@@ -417,7 +417,18 @@ let options5 = {
 
 
 
+// ******* ANIMATION STOPPER (stops animation while resizing) ********* //
 
+
+
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
 
 
 
