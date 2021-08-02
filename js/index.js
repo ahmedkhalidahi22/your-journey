a4ae418e94a29e9e6e49fee39c486ee7f062cd33
ahmedkhalidahi22 <79201796+ahmedkhalidahi22@users.noreply.github.com>
2021-08-03 00:31:22 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -282,3 +282,17 @@ let options3 = {
 
 
 //   *****************   ANIMATION FUNCTION   *****************   // 
+
+
+// ******* ANIMATION STOPPER (stops animation while resizing) ********* //
+
+
+
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
