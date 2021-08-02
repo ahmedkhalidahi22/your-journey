8efb8f4d7aa760a081021b056cf61e50c72a6ee9
ahmedkhalidahi22 <79201796+ahmedkhalidahi22@users.noreply.github.com>
2021-08-03 00:30:38 +0200

    Update all-des.js

--- js/all-des.js
+++ js/all-des.js
@@ -194,3 +194,18 @@ let grid = document.querySelector('.grid');
 let parent4 = document.querySelector('#hotels-sect');
 
  observer.observe(parent4);
+
+
+
+// ******* ANIMATION STOPPER (stops animation while resizing) ********* //
+
+
+
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
